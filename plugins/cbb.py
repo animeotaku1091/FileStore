49b120fba401359c2e1776e65912ffb0252a1d4d
animeotaku1091 <154030645+animeotaku1091@users.noreply.github.com>
2024-04-24 18:38:57 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-         text = f"<b>○ ᴏᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>ŦrαfͥαlͣgͫαrŁαw</a>\n○ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/animeotaku109'>ᴀɴɪᴍᴇ ᴏᴛᴀᴋᴜ </a>\n○ ᴀɴɪᴍᴇ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/teamotaku109'>ᴛᴇᴀᴍ ᴏᴛᴀᴋᴜ</a>\n○ ᴏɴɢᴏɪɴɢ ᴀɴɪᴍᴇ : <a href='https://t.me/ongoingotaku109'>ᴏɴɢᴏɪɴɢ ᴏᴛᴀᴋᴜ</a>\n○ ᴀɴɪᴍᴇ ᴄʜᴀᴛ : <a href='https://t.me/animeotakugc109'>ᴏᴛᴀᴋᴜ ᴄʜᴀᴛ</a></b>",
+          text = f"<b>○ ᴏᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>ŦrαfͥαlͣgͫαrŁαw</a>\n○ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/animeotaku109'>ᴀɴɪᴍᴇ ᴏᴛᴀᴋᴜ </a>\n○ ᴀɴɪᴍᴇ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/teamotaku109'>ᴛᴇᴀᴍ ᴏᴛᴀᴋᴜ</a>\n○ ᴏɴɢᴏɪɴɢ ᴀɴɪᴍᴇ : <a href='https://t.me/ongoingotaku109'>ᴏɴɢᴏɪɴɢ ᴏᴛᴀᴋᴜ</a>\n○ ᴀɴɪᴍᴇ ᴄʜᴀᴛ : <a href='https://t.me/animeotakugc109'>ᴏᴛᴀᴋᴜ ᴄʜᴀᴛ</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
